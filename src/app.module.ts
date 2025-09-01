f59438ca8d08dfaa8633f9ca7ff364e07adecc9e
Mehmet TAN aka B1-66ER <ce.mehmettan@gmail.com>
2025-09-01 20:56:00 +0300

    feat: Health module added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: process.env.SQLITE_PATH ?? 'emlak.db',
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
